7392681d57856e3a83acccce8cf54e00023d7473
John <19300336+papiJohn@users.noreply.github.com>
2017-12-17 19:13:37 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,36 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+<?php if ($this->is('index')) : ?>
+   <footer class="layoutSingleColumn layoutSingleColumn--wide footer" role="contentinfo"> 
+    <div class="site-info"> 
+     <p>blog since 2015.&nbsp;&nbsp;&nbsp;&nbsp; Theme <a href="https://github.com/papiJohn/Bigfa" target="_blank">Bigfa</a> 
+		by <a href="https://dearjohn.cn" target="_blank"><span class="cute">John</span></a>.&nbsp;&nbsp;&nbsp;&nbsp; 
+		CDN by <a href="https://console.upyun.com/register/?invite=rJoBowTv-" target="_blank"><span class="cute">又拍云</span></a>
+		&nbsp;&nbsp;&nbsp;&nbsp;苏ICP备17008143号</p>
+    </div>
+   </footer> 
+<?php else: ?>
+<footer class="footer--empty"></footer>
+<?php endif; ?>
+
+
+  </div> <!-- 对应site-main surface-container -->
+<div class="loadingBar"></div>
+
+
+
+<?php $this->footer(); ?>
+
+    <script src="https://cdn.bootcss.com/instantclick/3.0.0/instantclick.min.js" data-no-instant></script>
+    <script data-no-instant>
+      InstantClick.on('change', function(isInitialLoad) {
+        jQuery("time.timeago").timeago();
+        if (typeof Prism !== 'undefined'){
+          Prism.highlightAll(true,null);
+        }
+      });
+      InstantClick.init('mousedown');
+    </script>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,129 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+function themeConfig($form) {
+    $siteUrl = Helper::options()->siteUrl;
+    $nickname = new Typecho_Widget_Helper_Form_Element_Text('nickname', NULL, '', _t('侧边栏显示的昵称'), _t('显示在头像右侧'));
+    $form->addInput($nickname);
+
+    $descript = new Typecho_Widget_Helper_Form_Element_Text('descript', NULL, 'computer loser', _t(' 个人描述'), _t('将显示在侧边栏的昵称下方'));
+    $form->addInput($descript);
+
+    /** 设置CDN替换规则 */
+    $to_replace = new Typecho_Widget_Helper_Form_Element_Text('to_replace', NULL, '', _t('图片CDN替换前地址'), _t('如http://xxx.com'));
+    $form->addInput($to_replace);
+    $replace_to = new Typecho_Widget_Helper_Form_Element_Text('replace_to', NULL, '', _t('图片替换后地址'),_t('如https://cdn.xxx.com或//cdn.xxx.com'));
+    $form->addInput($replace_to);
+	
+	$next_cdn = new Typecho_Widget_Helper_Form_Element_Text('next_cdn', NULL, $siteUrl, _t('CDN 镜像地址'), _t('静态文件 CDN 镜像加速地址，加速js和css<br>格式参考：'.$siteUrl.'<br>不用请留空或者保持默认'));
+    $form->addInput($next_cdn);
+}
+
+function themeInit($archive) {
+    if ($archive->is('index')) {
+        $archive->parameter->pageSize = 8; // 自定义条数
+    }
+    if ($archive->is('categoty')) {
+        $archive->parameter->pageSize = 1000; // 自定义条数
+    }
+    if ($archive->is('single')) {
+        $archive->text = cdnReplace($archive);
+    }
+}
+
+//自定义评论
+function threadedComments($comments, $singleCommentOptions) {
+    $commentClass = '';
+    $commentLevelClass = $comments->_levels > 0 ? ' comment-child' : ' comment-parent';
+    ?>
+    <li class="comment">
+			<div id="<?php $comments->theId() ?>" class="comment-block">
+					<div class="comment-info u-clearfix">
+						<div class="comment-avatar">
+                            <?php
+                            //头像CDN
+                            $host = 'https://cdn.v2ex.com'; //自定义头像CDN服务器
+                            $url = '/gravatar/'; //自定义头像目录,一般保持默认即可
+                            $size = '32'; //自定义头像大小
+                            $rating = Helper::options()->commentsAvatarRating;
+                            $hash = md5(strtolower($comments->mail));
+                            $avatar = $host . $url . $hash . '?s=' . $size . '&r=' . $rating . '&d=';
+                            ?>
+                            <img class="avatar" src="<?php echo $avatar ?>" alt="<?php echo $comments->author; ?>" width="<?php echo $size ?>" height="<?php echo $size ?>" />
+							
+						</div>
+						<div class="comment-meta">
+							<div class="comment-author"><?php $singleCommentOptions->beforeAuthor();$comments->author();$singleCommentOptions->afterAuthor();?>
+									<span class="comment-reply-link u-cursorPointer" ><?php $comments->reply($singleCommentOptions->replyWord);?></span>
+							</div>
+							<div class="comment-time" itemprop="datePublished">
+                                <time class="timeago" datetime="<?php $comments->date('Y-m-d H:i:s');?>" itemprop="datePublished"><?php $comments->date('Y-m-d H:i:s');?></time>
+							</div>
+						</div>
+					</div>
+					<div class="comment-content">
+						<?php $comments->content();?>
+					</div>
+			</div>
+            <?php if ($comments->children) { ?>
+                <ol class="children">
+                    <?php $comments->threadedComments($singleCommentOptions);?>
+                </ol>
+            <?php } ?>
+    </li>
+    <?php
+}
+
+//获取文章首图或首个文件图
+function showfimg($cid)
+{
+		$position = 0;
+		$allowimg = 'jpg,bmp,png';//允许的图片格式
+		$imgurl = 'http://dearjohn.cn/usr/themes/Bigfa/img/default.jpg';//默认图片地址
+        $db = Typecho_Db::get();
+        $rs = $db->fetchRow($db->select('table.contents.text')
+        ->from('table.contents')
+        ->where('table.contents.type = ?', 'attachment')
+        ->where('table.contents.parent= ?', $cid)
+        ->order('table.contents.cid', Typecho_Db::SORT_ASC)
+        ->limit(1)->offset(max(0,intval($position)-1)));
+		if(isset($rs['text'])){
+			$fimg = unserialize($rs['text']);		
+			$imgtype = explode(",",$allowimg );
+			if(!in_array ($fimg['type'], $imgtype)) {
+				$fimg['path'] = $imgurl;
+			}
+			return $fimg['path'];
+		}else{
+			return $imgurl;
+		}
+}
+
+function getPostCount()
+{
+    $archives = Typecho_Widget::widget('Widget_Archive');
+    // 获取文章数目
+    $count = 0;
+    while ($archives->next()):
+        $count++;
+    endwhile;
+    return $count;
+}
+
+function cdnReplace(Widget_Archive $archive)
+{
+    $options = Typecho_Widget::widget('Widget_Options');
+    $to_replace = $options->to_replace;
+    $replace_to = $options->replace_to;
+    if(!empty($to_replace)&&!empty($replace_to)){
+        foreach($archive->stack as $index=>$con){
+            if(array_key_exists('text', $con)){
+                $archive->stack[$index]['text'] = str_replace($to_replace, $replace_to, $con['text']);
+            }
+        }
+        reset($archive->stack);
+        return str_replace($to_replace, $replace_to, $archive->text);
+    }else{
+        return $archive->text;
+    }
+}
