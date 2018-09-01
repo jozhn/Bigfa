86f7c25c1f444dedb2ca5897afcc96c3bb3629e5
jozhn <357721012@qq.com>
2018-09-01 09:34:59 +0800

    增加后台功能开关

--- footer.php
+++ footer.php
@@ -17,20 +17,25 @@
   </div> <!-- 对应site-main surface-container -->
 <div class="loadingBar"></div>
 <?php $this->footer(); ?>
-
-<!-- 生成文章目录树 -->
-<?php if (($this->options->tableOfContents == 'able') && ($this->is('post'))): ?>
-	<div id="directory-content" class="directory-content">
-		<div id="directory"></div>
-	</div>
-<?php endif; ?>
-
-    <script src="https://cdn.bootcss.com/instantclick/3.0.0/instantclick.min.js" data-no-instant></script>
+	<?php if ($this->options->highlightjs == 'able'):?>
 	<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/highlight.min.js" data-no-instant></script>
+	<?php endif; ?>
+	<?php if ($this->options->viewimage == 'able'):?>
 	<script src="https://img.dearjohn.cn/usr/themes/Bigfa/static/js/view-image.min.js" data-no-instant></script>
+	<?php endif; ?>
+	<?php if ($this->options->instantclick == 'able'):?>
+    <script src="https://cdn.bootcss.com/instantclick/3.0.0/instantclick.min.js" data-no-instant></script>
     <script data-no-instant>
 		InstantClick.on('change', function(isInitialLoad) {
 			$("pre code").each(function(i, block) {hljs.highlightBlock(block);});
+			<?php if ($this->options->aplayer == 'able'):?>
+			if (typeof aplayers !== 'undefined'){
+				for (var i = 0; i < aplayers.length; i++) {
+					try {aplayers[i].destroy()} catch(e){}
+				}
+			}
+			loadMeting();
+			<?php endif; ?>
 			jQuery(document).ready(function () {
 				$.lately({
 					'target' : '.lately-a,.lately-b,.lately-c'
@@ -44,5 +49,13 @@
 		});
 		InstantClick.init('mousedown');
     </script>
+	<?php endif; ?>
+	
+	<?php if($this->options->Analytics): ?>
+	<script>
+	<?php $this->options->Analytics(); ?>
+	</script>
+	<?php endif; ?>
+
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -3,32 +3,76 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 function themeConfig($form) {
     $siteUrl = Helper::options()->siteUrl;
+	
 	//昵称
     $nickname = new Typecho_Widget_Helper_Form_Element_Text('nickname', NULL, '', _t('侧边栏显示的昵称'), _t('显示在头像右侧'));
     $form->addInput($nickname);
+	
 	//头像
     $avatarUrl = new Typecho_Widget_Helper_Form_Element_Text('avatarUrl', NULL, '', _t('首页头像地址'), _t('将显示在首页侧边栏,如/usr/themes/Bigfa/img/head.jpg 或 https://xxx.com/xxx.jpg'));
     $form->addInput($avatarUrl);
+	
 	//个人描述
     $descript = new Typecho_Widget_Helper_Form_Element_Text('descript', NULL, 'computer loser', _t(' 个人描述'), _t('将显示在侧边栏的昵称下方'));
     $form->addInput($descript);
+	
 	//网站logo
     $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, '', _t('网站Logo地址'), _t('将显示在网站左上角'));
     $form->addInput($logoUrl);
+	
 	//文章默认缩略图
 	$thumUrl = new Typecho_Widget_Helper_Form_Element_Text('thumUrl', NULL, '', _t('文章默认缩略图地址'), _t('侧边栏文章默认缩略图地址'));
     $form->addInput($thumUrl);
+	
     //设置图片CDN替换规则
     $to_replace = new Typecho_Widget_Helper_Form_Element_Text('to_replace', NULL, '', _t('图片CDN替换前地址'), _t('如http://xxx.com'));
     $form->addInput($to_replace);
     $replace_to = new Typecho_Widget_Helper_Form_Element_Text('replace_to', NULL, '', _t('图片替换后地址'),_t('如https://cdn.xxx.com或//cdn.xxx.com'));
     $form->addInput($replace_to);
+	
     //静态资源CDN设置
     $next_cdn = new Typecho_Widget_Helper_Form_Element_Text('next_cdn', NULL, $siteUrl, _t('CDN 镜像地址'), _t('静态文件 CDN 镜像加速地址，加速js和css<br>格式参考：'.$siteUrl.'<br>不用请留空或者保持默认'));
     $form->addInput($next_cdn);
+	
+	//Highlightjs
+    $highlightjs = new Typecho_Widget_Helper_Form_Element_Radio('highlightjs',
+        array('able' => _t('启用'),
+            'disable' => _t('禁止'),
+        ),
+        'able', _t('Highlightjs代码高亮设置'), _t('默认启用'));
+    $form->addInput($highlightjs);
+	
+	//Viewimage
+    $viewimage = new Typecho_Widget_Helper_Form_Element_Radio('viewimage',
+        array('able' => _t('启用'),
+            'disable' => _t('禁止'),
+        ),
+        'able', _t('Viewimage灯箱设置'), _t('默认启用'));
+    $form->addInput($viewimage);
+	
+	//开启Instantclick
+    $instantclick = new Typecho_Widget_Helper_Form_Element_Radio('instantclick',
+        array('able' => _t('启用'),
+            'disable' => _t('禁止'),
+        ),
+        'able', _t('Instantclick设置'), _t('默认启用'));
+    $form->addInput($instantclick);
+	
+	//开启APlayer
+    $aplayer = new Typecho_Widget_Helper_Form_Element_Radio('aplayer',
+        array('able' => _t('启用'),
+            'disable' => _t('禁止'),
+        ),
+        'disable', _t('APlayer设置'), _t('默认禁止，若启用请先安装APlayer插件Meting版'));
+    $form->addInput($aplayer);
+	
 	//备案号
 	$beian = new Typecho_Widget_Helper_Form_Element_Text('beian', NULL, $siteUrl, _t('备案号'), _t('填写备案号'));
     $form->addInput($beian);
+	
+	//统计代码
+	$Analytics = new Typecho_Widget_Helper_Form_Element_Textarea('Analytics', NULL, NULL, _t('统计代码'), _t('填写你从谷歌Analytics或百度统计获取到的代码，不需要script标签'));
+    $form->addInput($Analytics);
 }
 
 function themeInit($archive) {
